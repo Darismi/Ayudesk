0225d21dcab8b3224b1bc57d8134e4798ae1aed7
Darismi <juan.elopez4265@gmail.com>
2020-03-10 23:10:06 -0500

    segundo commit

---
+++ conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+    $Host = "localhost";
+    $User = "root";
+    $Password = "";
+    $DB = "servicedeskdb";
+
+    $Conexion = mysqli_connect($Host,$User,$Password,$DB);
+
+    if($Conexion){
+        //echo "conexión exitosa";
+    }else{
+        echo "no se pudo conectar a la base de datos";
+    }
+
+?>
