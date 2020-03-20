e9eb994012a4f2aac5edafc0264a677dd8475b4c
Darismi <juan.elopez4265@gmail.com>
2020-03-19 20:13:59 -0500

    mejora de captura de incidentes

---
+++ captura_bd.php
@@ -0,0 +1,26 @@
+<?php
+
+ ?>
+
+ <table class="table table-bordered" style="margin-top: 50px;">
+   <th>
+     <td>
+       <h4>Dispositivo</h4>
+     </td>
+     <td>
+       <h4>Marca</h4>
+     </td>
+     <td>
+       <h4>Procesador</h4>
+     </td>
+     <td>
+       <h4>Memoria</h4>
+     </td>
+     <td>
+       <h4>Almacenamiento</h4>
+     </td>
+     <td>
+       <h4>sistema operativo</h4>
+     </td>
+   </th>
+ </table>
--- captura_incidentes.php
+++ captura_incidentes.php
@@ -1,3 +1,8 @@
+<?php
+  require 'conexion.php';
+  $query = mysqli_query($Conexion, "SELECT * FROM dispositivo");
+ ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -9,22 +14,51 @@
     <div class="container">
       <center>
         <h1>Reporte de incidente</h1>
-      </center>
 
-      <h2>Dispositivo afectado</h2>
-      <select name="select">
-        <option value="value1" selected >Dispositivo</option>
-        <option value="value2">Lenovo</option>
-        <option value="value3" >Asus</option>
-        <option value="value4">Acer</option>
-      </select>
-      <br>
-      <br>
-      <h2>¿Qué problema presenta?</h2>
-      <input type="text" name="" value="" style="width:500px; height:150px;" class="form-control">
-      <br>
-      <br>
-      <form class="" action="index.html" method="post">
+
+      <form class="" action="captura_bd.php" method="post">
+        <h2>Dispositivo afectado</h2>
+        <select name="select">
+          <option value="value1" selected >Dispositivo</option>
+          <?php
+            while ($row = $query -> fetch_row()) {
+            ?>
+                    <option value="<?=$row[0]?>"><?=$row[2]?></option>
+            <?php
+            }
+            ?>
+        </select>
+
+        <table class="table table-bordered" style="margin-top: 50px;">
+          <th>
+            <td>
+              <h4>Dispositivo</h4>
+            </td>
+            <td>
+              <h4>Marca</h4>
+            </td>
+            <td>
+              <h4>Procesador</h4>
+            </td>
+            <td>
+              <h4>Memoria</h4>
+            </td>
+            <td>
+              <h4>Almacenamiento</h4>
+            </td>
+            <td>
+              <h4>sistema operativo</h4>
+            </td>
+          </th>
+        </table>
+
+        <br>
+        <br>
+        <h2>¿Qué problema presenta?</h2>
+        <textarea placeholder="Escriba detalladamente" name="comentarios" rows="10" cols="70"></textarea>
+        <br>
+        <br>
+
         <h2>¿Ha intentado solucionar el problema?</h2>
         <br>
         <h3>Sí</h3>
@@ -35,12 +69,13 @@
         <br>
         <br>
         <h2>¿Cómo ha intentado solucionar el problema?</h2>
-        <input type="text" name="" value="" style="width:500px; height:150px;" class="form-control">
+        <textarea placeholder="Escriba detalladamente" name="comentarios" rows="10" cols="70"></textarea>
         <br>
         <br>
-        <input type="button" name="" value="Enviar" class="btn btn-default">
+        <input type="submit" class="btn btn-primary" placeholder="Reportar" name="btnIngresar">
       </form>
     </div>
+    </center>
     <script src="js/bootstrap.min.js" charset="utf-8"></script>
   </body>
 </html>
