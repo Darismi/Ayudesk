fb33bb8c309b4e764fd98ebaa33cfb28f9203a9f
Darismi <juan.elopez4265@gmail.com>
2020-04-10 14:08:54 -0500

    se agrego nav en algunas paginas y navegación para el usuario

--- captura_incidentes.php
+++ captura_incidentes.php
@@ -14,69 +14,87 @@
   </head>
   <body>
     <div class="container">
-      <center>
-        <h1>Reporte de incidente</h1>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Reportar incidente <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" href="mis_reportes.php">Mis reportes <span class="sr-only">(current)</span></a>
+            </li>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Salir</button>
+            </form>
+          </ul>
+        </div>
+      </nav>
 
+      <h1 class="mt-5">Reporte de incidente</h1>
 
-      <form class="" action="captura_bd.php" method="post">
-        <h2>Dispositivo afectado</h2>
-        <select name="dispositivo" required>
-          <option value="value1" selected >Dispositivo</option>
-          <?php
-            while ($row = $query -> fetch_row()) {
-            ?>
-              <option value="<?=$row[0]?>"><?=$row[2]?></option>
-            <?php
-            }
-            ?>
-        </select>
+        <div class="mt-5 bg-light">
 
-        <table class="table table-bordered" style="margin-top: 50px;">
-          <th>
-            <td>
-              <h5>Dispositivo</h5>
-            </td>
-            <td>
-              <h5>Marca</h5>
-            </td>
-            <td>
-              <h5>Procesador</h5>
-            </td>
-            <td>
-              <h5>Memoria</h5>
-            </td>
-            <td>
-              <h5>Almacenamiento</h5>
-            </td>
-            <td>
-              <h5>sistema operativo</h5>
-            </td>
-          </th>
-        </table>
+          <form class="" action="captura_bd.php" method="post">
+            <label for="">Dispositivo afectado</label>
+            <select class="custom-select .col-" name="dispositivo" required>
+              <option value="value1" selected >Dispositivo</option>
+              <?php
+                while ($row = $query -> fetch_row()) {
+                ?>
+                  <option value="<?=$row[0]?>"><?=$row[2]?></option>
+                <?php
+                }
+                ?>
+            </select>
 
-        <br>
-        <br>
-        <h2>¿Qué problema presenta?</h2>
-        <textarea placeholder="Escriba detalladamente" name="problema" rows="10" cols="70" required></textarea>
-        <br>
-        <br>
+            <table class="table table-bordered mt-5">
+              <th>
+                <td>
+                  <h5>Dispositivo</h5>
+                </td>
+                <td>
+                  <h5>Marca</h5>
+                </td>
+                <td>
+                  <h5>Procesador</h5>
+                </td>
+                <td>
+                  <h5>Memoria</h5>
+                </td>
+                <td>
+                  <h5>Almacenamiento</h5>
+                </td>
+                <td>
+                  <h5>sistema operativo</h5>
+                </td>
+              </th>
+            </table>
 
-        <h2>¿Ha intentado solucionar el problema?</h2>
-        <br>
-        <h3>Sí</h3>
-        <input type="radio" name="group" value="Si">
-        <h3>No</h3>
-        <input type="radio" name="group" value="No">
-        <br>
-        <br>
-        <h2>¿Cómo ha intentado solucionar el problema?</h2>
-        <textarea placeholder="Escriba detalladamente" name="intento" rows="10" cols="70"></textarea>
-        <br>
-        <br>
-        <input type="submit" class="btn btn-primary" placeholder="Reportar" value="Reportar" name="btnIngresar">
-      </form>
+            <br>
+            <br>
+            <h2>¿Qué problema presenta?</h2>
+            <textarea placeholder="Escriba detalladamente" name="problema" rows="10" cols="70" required></textarea>
+            <br>
+            <br>
+
+            <h2>¿Ha intentado solucionar el problema?</h2>
+            <br>
+            <label for="">Sí</label>
+            <input type="radio" name="group" value="Si">
+            <label for="">No</label>
+            <input type="radio" name="group" value="No">
+            <br>
+            <br>
+            <h2>¿Cómo ha intentado solucionar el problema?</h2>
+            <textarea placeholder="Escriba detalladamente" name="intento" rows="10" cols="70"></textarea>
+            <br>
+            <br>
+            <input type="submit" class="btn btn-primary" placeholder="Reportar" value="Reportar" name="btnIngresar">
+          </form>
+        </div>
     </div>
-    </center>
+
     <script src="js/bootstrap.min.js" charset="utf-8"></script>
   </body>
 </html>
