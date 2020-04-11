addd97caf3e30b1bfa9d1e91a672531b462d3d32
Darismi <juan.elopez4265@gmail.com>
2020-04-10 21:01:24 -0500

    se agregó mis reportes

---
+++ mis_reportes.php
@@ -0,0 +1,62 @@
+<?php
+  require 'conexion.php';
+  session_start();
+  $query = mysqli_query($C_reportes, "SELECT * FROM reporte WHERE id_usuario = '".$_SESSION['idusuario']."'");
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <link rel="stylesheet" href="css/bootstrap.css">
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/bootstrap-grid.css">
+     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+     <link rel="stylesheet" href="css/bootstrap-reboot.css">
+     <meta charset="utf-8">
+     <title>Service Desk</title>
+   </head>
+   <body>
+     <div class="container">
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           <ul class="navbar-nav mr-auto">
+             <li class="nav-item">
+               <a class="nav-link" href="captura_incidentes.php">Reportar incidente <span class="sr-only">(current)</span></a>
+             </li>
+             <li class="nav-item active">
+               <a class="nav-link" href="mis_reportes.php">Mis reportes <span class="sr-only">(current)</span></a>
+             </li>
+             <form class="form-inline my-2 my-lg-0">
+               <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Salir</button>
+             </form>
+           </ul>
+         </div>
+       </nav>
+       <table class="table table-bordered mt-5">
+         <thead>
+           <tr>
+             <th><h2> Fecha </h2></th>
+             <th><h2> Descripción </h2></th>
+             <th><h2> Estado </h2></th>
+           </tr>
+         </thead>
+          <tbody>
+            <?php
+            while ($row = $query->fetch_row()) {
+            ?>
+
+              <tr>
+                <td ><?=$row[1]?></td>
+                <td ><?=$row[2]?></td>
+                <td><?=$row[12]?></td>
+              </tr>
+              <?php
+            }
+             ?>
+          </tbody>
+       </table>
+     </div>
+
+   </body>
+ </html>
